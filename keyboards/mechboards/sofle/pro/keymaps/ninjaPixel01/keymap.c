0a8ce0b930dd314628a2877bb69f5997ef6be2f1
Matt Michel <matt@ninjaPixel.io>
2026-03-13 16:18:55 +0000

    moor keys inc RGB

--- keyboards/mechboards/sofle/pro/keymaps/ninjaPixel01/keymap.c
+++ keyboards/mechboards/sofle/pro/keymaps/ninjaPixel01/keymap.c
@@ -24,14 +24,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       KC_LCTL, KC_LALT, KC_LGUI, MT(MOD_RSFT, KC_ENT), KC_HYPR,     KC_MEH,  LT(1, KC_SPC),  KC_RGUI, KC_RALT,  KC_RCTL
   ),
   [1] = LAYOUT(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                      KC_F6,   KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_F11,
+    TG(2), KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                      KC_F6,   KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_F11,
     KC_GRV,  KC_X,    KC_LBRC,    KC_LPRN,    KC_LCBR,    KC_GRV,                            _______,    KC_RCBR,    KC_RPRN,     KC_RBRC,    KC_SEMICOLON,    KC_F12,
-    CW_TOGG, LALT(KC_DEL), KC_DEL,   KC_QUOTE, KC_SEMICOLON,  _______,                                    KC_MINUS, KC_LEFT, KC_DOWN,  KC_UP,   KC_RIGHT, KC_PIPE,
-    _______, KC_EQL,  KC_MINS, KC_PLUS, KC_EQL, KC_RCBR,  _______,                 _______, LGUI(KC_LEFT), KC_PGDN, KC_PGUP,  LGUI(KC_LEFT), KC_BSLS, _______,
+    CW_TOGG, LALT(KC_DEL), KC_DEL,   KC_QUOTE, KC_SEMICOLON,  _______,                       KC_MINUS, KC_LEFT, KC_DOWN,  KC_UP,   KC_RIGHT, KC_PIPE,
+    _______, KC_EQL,  KC_MINS, KC_PLUS, KC_EQL, KC_RCBR,  _______,                 _______, _______, LGUI(KC_LEFT), KC_PGDN, KC_PGUP,  LGUI(KC_LEFT), _______,
                       _______, _______, _______, _______,  _______,                 _______, _______,   _______, _______,  _______
   ),
   [2] = LAYOUT(
-    _______, _______, _______, _______, _______, _______,                          _______, _______, _______, _______, _______ ,_______,
+    TO(0),   RGB_MOD, RGB_RMOD, RGB_TOG, RGB_VAI, RGB_VAD,                          RGB_SPI, RGB_SPD, _______, _______, _______ ,_______,
     _______, KC_INS,  KC_PSCR, KC_APP,  XXXXXXX, XXXXXXX,                          KC_PGUP, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, KC_BSPC,
     _______, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX, KC_CAPS,                          KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,  XXXXXXX,
     _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX, _______,        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
