e808e695f3c34b0f510ba6255ce8cf4c865980b8
Matt Michel <matt@ninjaPixel.io>
2026-03-13 15:39:57 +0000

    getting there

--- keyboards/mechboards/sofle/pro/keymaps/ninjaPixel01/keymap.c
+++ keyboards/mechboards/sofle/pro/keymaps/ninjaPixel01/keymap.c
@@ -18,16 +18,16 @@ tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                       KC_6,           KC_7,    KC_8,     KC_9,   KC_0,       LALT(KC_TAB),
-    TD(TD_NOODLE),  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                       KC_J,           KC_L,    KC_U,     KC_Y,   KC_BSLS,    LALT(KC_BSPC),
+    TD(TD_NOODLE),  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                KC_J,           KC_L,    KC_U,     KC_Y,   KC_BSLS,    LALT(KC_BSPC),
     KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                       KC_M,           KC_N,    KC_E,     KC_I,   KC_O,       KC_BSPC,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                 KC_MUTE,     KC_MPLY, KC_K,           KC_H,    KC_COMMA, KC_DOT, KC_SLSH,    KC_LSFT,
                       KC_LCTL, KC_LALT, KC_LGUI, MT(MOD_RSFT, KC_ENT), KC_HYPR,     KC_MEH,  LT(1, KC_SPC),  KC_RGUI, KC_RALT,  KC_RCTL
   ),
   [1] = LAYOUT(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                      KC_F6,   KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_F11,
-    KC_GRV,  KC_X,    KC_2,    KC_3,    KC_4,    KC_GRV,                                       KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_F12,
-    CW_TOGG, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                    KC_CIRC, KC_LEFT, KC_DOWN,  KC_UP,   KC_RIGHT, KC_PIPE,
-    _______, KC_EQL,  KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR,  _______,                 _______, KC_LBRC, KC_RBRC, KC_SCLN,  KC_COLN, KC_BSLS, _______,
+    KC_GRV,  KC_X,    KC_LBRC,    KC_LPRN,    KC_LCBR,    KC_GRV,                            _______,    KC_RCBR,    KC_RPRN,     KC_RBRC,    KC_SEMICOLON,    KC_F12,
+    CW_TOGG, LALT(KC_DEL), KC_DEL,   KC_QUOTE, KC_SEMICOLON,  _______,                                    KC_MINUS, KC_LEFT, KC_DOWN,  KC_UP,   KC_RIGHT, KC_PIPE,
+    _______, KC_EQL,  KC_MINS, KC_PLUS, KC_EQL, KC_RCBR,  _______,                 _______, LGUI(KC_LEFT), KC_PGDN, KC_PGUP,  LGUI(KC_LEFT), KC_BSLS, _______,
                       _______, _______, _______, _______,  _______,                 _______, _______,   _______, _______,  _______
   ),
   [2] = LAYOUT(
@@ -48,8 +48,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-[0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT)},
-[1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
+[0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(LSG(KC_LBRC), LSG(KC_RBRC))},
+[1] = { ENCODER_CCW_CW(KC_MPRV, KC_MNXT), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
 [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
 [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)}
 };
