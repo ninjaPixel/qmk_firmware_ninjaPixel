df069e5c87fc3e40b22e3488c9137fd399e791f9
Matt Michel <matt@ninjaPixel.io>
2026-03-16 13:10:22 +0000

    claude refactor to share keymaps by using user spaces

--- keyboards/sofle/keymaps/ninjaPixel_sofle_rev1/rules.mk
+++ keyboards/sofle/keymaps/ninjaPixel_sofle_rev1/rules.mk
@@ -1,10 +1,8 @@
-# Enable rotary encoder map (per-layer encoder bindings)
+# Point to the shared ninjaPixel userspace (users/ninjaPixel/)
+USER_NAME := ninjaPixel
+
 ENCODER_MAP_ENABLE = yes
-
-# Enable tap dance (used for TD_NOODLE on the far-left key of row 1)
 TAP_DANCE_ENABLE = yes
-
-# Enable caps word (CW_TOGG on layer 2)
 CAPS_WORD_ENABLE = yes
 
 # Override bootloader for Elite-C v4 (uses Atmel DFU, not Caterina like Pro Micro)
